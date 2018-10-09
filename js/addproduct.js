36eebf1d371ac097a88302a96735bce0099a4155
NobeddBroccoli <cyq3311@gmail.com>
2018-10-09 17:12:02 +0800

    Login added, js code refractored

--- js/addproduct.js
+++ js/addproduct.js
@@ -1,1 +1,39 @@
+var db = firebase.firestore();
+    const inputProductName = document.querySelector("#productName");
+    const inputProductPrice = document.querySelector("#productPrice");
+    const inputProductRawPrice = document.querySelector("#productRawPrice");
+    const inputProductCategory = document.querySelector("#productCategory");
 
+    saveProductButton.addEventListener("click", function(){
+        const SaveProductName = inputProductName.value;
+        const SaveProductPrice = inputProductPrice.value;
+        const SaveProductRawPrice = inputProductRawPrice.value;
+        const SaveProductCategory = inputProductCategory.value;
+        db.collection("Products").add({
+            PName: SaveProductName,
+            Sales_Price: SaveProductPrice,
+            Raw_Price: SaveProductRawPrice,
+            Category: SaveProductCategory
+        })
+        .then(function(docRef){
+            console.log("Product saved!", docRef.id);
+        })
+        .catch(function(error){
+            console.log("Got an error: ", error);
+        }); 
+    });
+
+
+    const inputCategoryName = document.querySelector("#categoryName") 
+    saveCategoryButton.addEventListener("click", function(){
+        const SaveCategoryName = inputCategoryName.value;
+        db.collection("Categories").add({
+            Category: SaveCategoryName
+        })
+        .then(function(docRef){
+            console.log("Category saved!", docRef.id);
+        })
+        .catch(function(error){
+            console.log("Got and eror: ", error);
+        });
+    });
--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 //(function(){
-    // Initialize Firebase
     var config = {
-    apiKey: "AIzaSyAd_KsL66EQmcNGlecBSYBOJMieoxi5Qyk",
-    authDomain: "dp2passgroup.firebaseapp.com",
-    databaseURL: "https://dp2passgroup.firebaseio.com",
-    projectId: "dp2passgroup",
-    storageBucket: "dp2passgroup.appspot.com",
-    messagingSenderId: "1032158016281"
+        apiKey: "AIzaSyAd_KsL66EQmcNGlecBSYBOJMieoxi5Qyk",
+        authDomain: "dp2passgroup.firebaseapp.com",
+        databaseURL: "https://dp2passgroup.firebaseio.com",
+        projectId: "dp2passgroup",
+        storageBucket: "dp2passgroup.appspot.com",
+        messagingSenderId: "1032158016281"
     };
-    firebase.initializeApp(config);
+        firebase.initializeApp(config);
 
     loginButton.addEventListener("click", function(){
         firebase.auth().signInAnonymously();
@@ -21,33 +20,11 @@
     firebase.auth().onAuthStateChanged(function(user){
         console.log(user);
         if(user){
-            document.getElementById("signoutButton").style.visibility = "show"
+            console.log("you have signed in")
+            document.getElementById("signoutButton").style.visibility = "visible"
         } else{
+            console.log("you have signed off");
             document.getElementById("signoutButton").style.visibility = "hidden"
         }
     })
-
-    var db = firebase.firestore();
-    const outputHeader = document.querySelector("#addProduct");
-    const inputProductName = document.querySelector("#productName");
-    const inputProductPrice = document.querySelector("#productPrice");
-    const inputProductRawPrice = document.querySelector("#productRawPrice");
-
-    saveButton.addEventListener("click", function(){
-        const SaveProductName = inputProductName.value;
-        const SaveProductPrice = inputProductPrice.value;
-        const SaveProductRawPrice = inputProductRawPrice.value;
-        db.collection("Products").add({
-            ProductName: SaveProductName,
-            ProductPrice: SaveProductPrice,
-            ProductRawPrice: SaveProductRawPrice
-        })
-        .then(function(docRef){
-            console.log("Product saved!", docRef.id);
-        })
-        .catch(function(error){
-            console.log("Got an error: ", error);
-            }); 
-    });
-
 //})

acf3a901ee5d3b5faa490e873d5404f7b0207aee
QinusAxia <warframeaxia@gmail.com>
2018-10-15 14:04:40 +0800

    when table row clicked it will highlight

--- js/inventoryMain.js
+++ js/inventoryMain.js
@@ -1,17 +1,32 @@
 var db = firebase.firestore();
 
+document.getElementById("Edit").disabled=true;
+document.getElementById("DeleteProduct").disabled=true;
+
+//This function generates the list of products and put into a table form
 db.collection("Products")
 .get()
 .then(function(querySnapshot){     
-    var content = "";
+    var content = "";   
     querySnapshot.forEach (function(doc){
         content += '<tr>';
         content += '<td>' + doc.data().PName + '</td>';
         content += '<td>' + doc.data().Category + '</td>';
         content += '<td>' + doc.data().Raw_Price + '</td>';
         content += '<td>' + doc.data().Sales_Price + '</td>';
-        content += '<td>' + '<Button>Edit</Button><Button>Delete</Button>' + '</td>';
+        //content += '<td>' + '<Button>Edit</Button><Button>Delete</Button>' + '</td>';
         content += '</tr>';
 })
 $('#table').append(content);
 })
+
+//function to highligt row when clicked
+$(document).ready(
+setTimeout(function(){
+    $("#table tr").click(function() {
+        var selected = $(this).hasClass("highlight");
+        $("#table tr").removeClass("highlight");
+        if(!selected)
+                $(this).addClass("highlight");
+    });
+}, 3000));

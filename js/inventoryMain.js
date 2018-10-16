1582cd11eacd2da1fbe7e4626d30974376b3389e
QinusAxia <warframeaxia@gmail.com>
2018-10-16 10:22:21 +0800

    commiting inventoryMain

--- js/inventoryMain.js
+++ js/inventoryMain.js
@@ -1,20 +1,21 @@
-var db = firebase.firestore();
+var database = firebase.firestore();
 
 document.getElementById("Edit").disabled=true;
 document.getElementById("DeleteProduct").disabled=true;
 
 //This function generates the list of products and put into a table form
-db.collection("Products")
+database.collection("Products")
 .get()
 .then(function(querySnapshot){     
     var content = "";   
     querySnapshot.forEach (function(doc){
         content += '<tr>';
+        // content += '<td>' + doc.get + '</td>'
         content += '<td>' + doc.data().PName + '</td>';
         content += '<td>' + doc.data().Category + '</td>';
         content += '<td>' + doc.data().Raw_Price + '</td>';
         content += '<td>' + doc.data().Sales_Price + '</td>';
-        //content += '<td>' + '<Button>Edit</Button><Button>Delete</Button>' + '</td>';
+        content += '<td>' + '<Button>Edit</Button><Button>Delete</Button>' + '</td>';
         content += '</tr>';
 })
 $('#table').append(content);

87d4589303254c820ee8d1d6174ba9dadd4a136e
NobeddBroccoli <cyq3311@gmail.com>
2018-10-16 23:28:13 +0800

    Add Sales submodule completed

--- js/addSales.js
+++ js/addSales.js
@@ -1,21 +1,20 @@
-//TODO PRICE LOGIC
-
 $(document).ready(function(){
     var db = firebase.firestore();
-    var map = {};
+    var mapPrice = {};
     var totalPrice;
+    var formTableRowCount = 0;
     
     //Populate Map for ProductName : SellingPrice
     db.collection("Products")
     .get()
     .then(function(querySnapshot){
         querySnapshot.forEach(function(doc){
-            map[doc.data().PName] = doc.data().Sales_Price;
+            mapPrice[doc.data().PName] = doc.data().Sales_Price;
         })
     })
 
-    //Get products for dropdown
-    function getProductNames(i){
+    //Populate select dropdown with products function
+    function populateSelectDropdownWithProducts(i){
         if (i === undefined)
             i = "";
 
@@ -31,45 +30,76 @@ $(document).ready(function(){
         $('#dropdownDetails' + i).append(content);
         })
     }
-    //For the first row 
-    getProductNames(0);
+    //Populate the first row in view
+    populateSelectDropdownWithProducts(0);
 
 
     //Click function for add button
     var i = 0;
     $('#add').click(function(){
-        i++;
-        var content = "";
-        content += '<tr><td> ' + (i+1) + '</td>'
-        content += '<td><select id="dropdownDetails' + i + '"><option value="0">Choose a product to add</option></select></td>'
-        content += '<td><input type="number" id="inputDetailsQuantity' + i + '"/></td>';
+        var content = ""; 
+        content += '<tr id="' + i + '">'
+        content += '<td class="onChangeUpdatePrice"><select id="dropdownDetails' + i + '" class="custom-select" required><option value="">Select a product to add</option></select></td>'
+        content += '<td class="onChangeUpdatePrice"><input type="number" id="inputDetailsQuantity' + i + '" class="form-control" placeholder="Enter quantity" min="1" required /></td>';
+        content += '<td class="unitPrice" id="unitPrice' + i + '"></td>';
+        content += '<td class="totalUnitPrice" id="totalUnitPrice' + i + '"></td>';
         content += '<td><button id="remove" class="btn btn-danger remove">Remove</button></td></tr>';
         $('#dynamic_SalesDetailsField').append(content);
-        getProductNames(i);
+        populateSelectDropdownWithProducts(i);
+        
+        //add button is disabled when its clicked 15 times
+        formTableRowCount++;
+        if(formTableRowCount == 15)
+            $("#add").attr('disabled','disabled');
     });
-
+    
+    //Remove function for remove button
     $(document).on('click', '.remove', function(){
         $(this).closest('tr').remove();
     });
 
+    //Update price logic, listens to all onChangeUpdatePrice class
+    $(document).on('change', '.onChangeUpdatePrice', function(){
+        var rowID = $(this).closest('tr').attr('id');
+        var selectedOptionsDropdown = $("#dropdownDetails" + rowID +" option:selected").text();
+        var quantity = document.querySelector("#inputDetailsQuantity" + rowID).value;
+        var unitPrice = parseFloat(mapPrice[selectedOptionsDropdown]);
+        var totalUnitPrice = unitPrice * quantity;
+        
+        $(this).siblings(".unitPrice").text(unitPrice);
+        $(this).siblings(".totalUnitPrice").text(totalUnitPrice);
 
-    //Click function for save sales button
-    saveSales.addEventListener("click", function(){
+        totalPrice = 0;
+        $(".totalUnitPrice").each(function(){
+            totalPrice += parseFloat($(this).text());
+        })
+        $("#totalPrice").text(totalPrice);  
+    });
+
+    //onSubmit function
+    $("#addSalesForm").submit(function(event){
         var SaveSalesDetailsID = [];
         var SaveDetails = [];
-        var i = 0;
-
-        for(i = 0; i < 10; i++){
-            if($("#dropdownDetails" + i +" option:selected").text() != "")
-                SaveDetails.push({Name: $("#dropdownDetails" + i +" option:selected").text(), Quantity: document.querySelector("#inputDetailsQuantity" + i).value});
+        
+        var rowID = 0;
+        for(rowID = 0; rowID < formTableRowCount+1; rowID++){ 
+            if($("#dropdownDetails" + rowID +" option:selected").text() != ""){
+                SaveDetails.push({
+                    name: $("#dropdownDetails" + rowID +" option:selected").text(), 
+                    quantity: parseInt($("#inputDetailsQuantity" + rowID).val()), 
+                    unitPrice: parseFloat($("#unitPrice" + rowID).text()), 
+                    totalUnitPrice: parseFloat($("#totalUnitPrice" + rowID).text())
+                });
+            }
+                
         }
 
         SaveDetails.forEach(function(details, index){
-            
-
             db.collection("SalesDetails").add({
-                ProductName: details.Name,
-                Quantity: details.Quantity
+                ProductName: details.name,
+                Quantity: details.quantity,
+                UnitPrice: details.unitPrice,
+                TotalUnitPrice: details.totalUnitPrice
             })
             .then(function(docRef){
                 console.log("SD success!", docRef.id);
@@ -77,19 +107,22 @@ $(document).ready(function(){
 
                 if(index == SaveDetails.length-1){
                     db.collection("SalesRecord").add({
-                        IDs: SaveSalesDetailsID
+                        IDs: SaveSalesDetailsID,
+                        TotalPrice: totalPrice,
+                        Date: new Date()
                     })
                     .then(function(docRef){
                         console.log("SR success!", docRef.id)
                     })
                     .catch(function(error){
-                        console.log("error: ", error)
+                        console.log("SR error: ", error)
                     });
                 }
             })
             .catch(function(error){
-                console.log("error: ", error);
-            });        
+                console.log("SD error: ", error);
+            });   
+
         });   
     });
 

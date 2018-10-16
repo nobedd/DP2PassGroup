e6a9f41b7d21dc9aa1e74f58d3529c5bfdecf50f
QinusAxia <warframeaxia@gmail.com>
2018-10-16 15:55:22 +0800

    Completed the delete product function! Now proceeding to edit product

--- js/inventoryMain.js
+++ js/inventoryMain.js
@@ -1,33 +1,46 @@
-var database = firebase.firestore();
+var db = firebase.firestore();
 
 document.getElementById("Edit").disabled=true;
 document.getElementById("DeleteProduct").disabled=true;
 
 //This function generates the list of products and put into a table form
-database.collection("Products")
+db.collection("Products")
 .get()
-.then(function(querySnapshot){     
+.then(function(querySnapshot){
+    var productIDarray = {};
+    var IDcounterForEachProduct=0;
     var content = "";   
     querySnapshot.forEach (function(doc){
-        content += '<tr>';
-        // content += '<td>' + doc.get + '</td>'
+        productIDarray[IDcounterForEachProduct]=doc.id;//mapping the index number to ID
+        content += '<tr id="' + IDcounterForEachProduct + '">';
         content += '<td>' + doc.data().PName + '</td>';
         content += '<td>' + doc.data().Category + '</td>';
         content += '<td>' + doc.data().Raw_Price + '</td>';
         content += '<td>' + doc.data().Sales_Price + '</td>';
-        content += '<td>' + '<Button>Edit</Button><Button>Delete</Button>' + '</td>';
+        content += '<td>' + '<Button class="editProduct">Edit</Button><Button class="deleteProduct">Delete</Button>' + '</td>';
         content += '</tr>';
+        IDcounterForEachProduct++;
+});
+
+$(document).on("click", ".deleteProduct", function(){
+    console.log("deleted button clicked");//works
+    var getrowIndex = $(this).closest('tr').attr('id');
+    console.log(getrowIndex);//works
+    var getPID = productIDarray[getrowIndex];
+    console.log(getPID);//works
+    $(this).closest('tr').remove();
+
+    //now i have to delete the document with getPID from the database
+    db.collection("Products").doc(getPID).delete().then(function() {
+        console.log("Document successfully deleted!");
+        
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+    //then triger a function that will delete the entire <tbody> and then reload it
+
 })
 $('#table').append(content);
 })
 
-//function to highligt row when clicked
-$(document).ready(
-setTimeout(function(){
-    $("#table tr").click(function() {
-        var selected = $(this).hasClass("highlight");
-        $("#table tr").removeClass("highlight");
-        if(!selected)
-                $(this).addClass("highlight");
-    });
-}, 3000));
+

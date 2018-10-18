921d4da6e4f4aa4716ab7e0ac036fa981e9be2d9
QinusAxia <warframeaxia@gmail.com>
2018-10-18 17:03:00 +0800

    committing to pull

--- js/editproduct.js
+++ js/editproduct.js
@@ -1,18 +1,23 @@
 var db = firebase.firestore();
 
 var ProductID = localStorage.getItem("PID");
-localStorage.clear();
-document.getElementById("textID").innerHTML = ProductID;
+var ProductName;
+var ProductSalePrice;
+var ProductRawprice;
+//var productCategory;localStorage.clear();
+//document.getElementById("textID").innerHTML = ProductID;
 
-var docRefName = db.collection("Products").doc(ProductID);
-Pname = docRefName.get();
-// var productRawPrice=db.collection("Products").doc(ProductID).PName;
-// var productSalePrice=db.collection("Products").doc(ProductID).PName;
-// var productCategory=db.collection("Products").doc(ProductID).PName;
-
-
+db.collection("Products").doc(ProductID)
+    .get()
+    .then(function(doc){
+        ProductName = doc.data().PName;
+        document.getElementById("textID").innerHTML = ProductID;
+    })
+    .catch(function(error){
+        alert("Hey ERROR", error);
+});
 
 $(document).on("click","#showPID", function(){
-    alert("The product name is" + productName);
+    alert("The product name is" + ProductName);
 })
 

285402043cdbf57c3d47fe8aafc3345d7fb36996
NobeddBroccoli <cyq3311@gmail.com>
2018-10-22 13:22:15 +0800

    Generate report table ver 1

--- js/generateReport.js
+++ js/generateReport.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     var db = firebase.firestore();
     var ProductsList = [];
+    var startDate;
+    var endDate
 
     db.collection("Products").orderBy("PName", "asc")
     .get()
@@ -24,39 +26,64 @@ $(document).ready(function(){
         $('#dropdownCategory').append(content);
     });
 
-    db.collection("Products").orderBy("PName", "asc")
-    .get()
-    .then(function(querySnapshot){
-        querySnapshot.forEach(function(doc){
-            ProductsList.push(doc.data());
-        })
-
-        ProductsList.forEach(function(ProductDetails){
-            // console.log(ProductDetails);
-            db.collection("SalesDetails").where("ProductName", "==", ProductDetails.PName) 
-            .get()
-            .then(function(querySnapshot){
-                var quantitySold = 0;
-                var revenue = 0;
-                var content = "";
-                querySnapshot.forEach(function(doc){
-                    quantitySold += doc.data().Quantity;
-                })
-                revenue = ProductDetails.Sales_Price * quantitySold
-                content += '<tr>';
-                content += '<td>' + ProductDetails.PName + '</td>';
-                content += '<td>' + ProductDetails.Category + '</td>';
-                content += '<td>' + ProductDetails.Sales_Price + '</td>';
-                content += '<td>' + quantitySold + '</td>';
-                content += '<td>' + revenue + '</td>';
-                content += '</tr>';
-
-                $('#table').append(content);
-            })
-        })
+    $(document).on("change", "#startDate", function(){
+        startDate = new Date($(this).val());
     })
 
+    $(document).on("change", "#endDate", function(){
+        endDate = new Date($(this).val());
+    })
+
+    $('#generateTableButton').click(function(){
+
+        db.collection("Products").orderBy("PName", "asc")
+        .get()
+        .then(function(querySnapshot){
+            var totalQuantity=0, totalRevenue=0, totalGrossMargin=0;
+            querySnapshot.forEach(function(doc){
+                ProductsList.push(doc.data());
+            })
+    
+            ProductsList.forEach(function(ProductDetails, index){
+                // console.log(ProductDetails);
+                db.collection("SalesDetails").where("ProductName", "==", ProductDetails.PName).where("Date", ">=", startDate).where("Date", "<=", endDate)
+                .get()
+                .then(function(querySnapshot){
+                    var quantitySold=0, revenue=0, grossMargin=0;
+                    var content = "";
+                    querySnapshot.forEach(function(doc){
+                        quantitySold += doc.data().Quantity;
+                    })
+                    if(quantitySold != 0){
+                        revenue = ProductDetails.Sales_Price * quantitySold;
+                        grossMargin = revenue - (ProductDetails.Raw_Price * quantitySold);
+                        totalQuantity += quantitySold;
+                        totalRevenue += revenue;
+                        totalGrossMargin += grossMargin;
+                        content += '<tr>';
+                        content += '<td>' + ProductDetails.PName + '</td>';
+                        content += '<td>' + ProductDetails.Category + '</td>';
+                        content += '<td>' + ProductDetails.Sales_Price + '</td>';
+                        content += '<td>' + quantitySold + '</td>';
+                        content += '<td>' + revenue + '</td>';
+                        content += '<td>' + grossMargin + '</td>';
+                        content += '</tr>';
+        
+                        $('#table').append(content);
+                    }
+                    if(index == ProductsList.length-1){
+                        $("#totalQuantity").text(totalQuantity); 
+                        $("#totalRevenue").text(totalRevenue); 
+                        $("#totalGrossMargin").text(totalGrossMargin); 
+                    }
+                }).catch(function(error){
+                    alert(error);
+                })     
+            })
+        })
 
 
+    })
+    
     
 });

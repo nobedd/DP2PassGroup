17da73d1bfeecb56f72f401253b74213af713f62
QinusAxia <warframeaxia@gmail.com>
2018-10-23 13:07:11 +0800

    Completed delete category function. Added a NIL category that should not be deleted.

--- js/addproduct.js
+++ js/addproduct.js
@@ -4,27 +4,28 @@
     const inputProductName = document.querySelector("#productName");
     const inputProductPrice = document.querySelector("#productPrice");
     const inputProductRawPrice = document.querySelector("#productRawPrice");
-    
 
-
-    //Populate select dropdown with products function
+    //Populate 2 select dropdown with products function
     function populateSelectDropdownCategory(){
-        db.collection("Categories")
+        db.collection("Categories").orderBy("Category","asc")
         .get()
-        .then(function(querySnapshot){     
+        .then(function(querySnapshot){
+            var IDcounterForEachProduct="";
             var content = "";
-            var value = 1;
             querySnapshot.forEach (function(doc){
-                content += "<option value='" + value + "'>" + doc.data().Category + "</option>";
-                value++;
+                IDcounterForEachProduct=doc.id;
+                content += "<option value='" + IDcounterForEachProduct + "'>" + doc.data().Category + "</option>";
             })
+            $('#dropdownCategory').empty(content);
             $('#dropdownCategory').append(content);
+            $('#categoryDeleteDrop').empty(content);
+            $('#categoryDeleteDrop').append(content);
         })
     }
     //Populate the first row in view
     populateSelectDropdownCategory();
 
-
+    //Button to Save the products
     saveProductButton.addEventListener("click", function(){
         const SaveProductName = inputProductName.value;
         const SaveProductPrice = inputProductPrice.value;
@@ -46,42 +47,79 @@
         }); 
     });
 
-    // Below is just to display the table
-    var productIDs = [];
-    db.collection("Products")
-        .get()
-        .then(function(querySnapshot){     
-            var content = "";
-            querySnapshot.forEach (function(doc){
-                content += '<tr>';
-                content += '<td>' + doc.data().PName + '</td>';
-                content += '<td>' + doc.data().Category + '</td>';
-                content += '<td>' + doc.data().Raw_Price + '</td>';
-                content += '<td>' + doc.data().Sales_Price + '</td>';
-                // content += '<td>' + '<Button>Edit</Button>' + '</td>';
-                content += '</tr>';
-        })
-        $('#table').append(content);
-    })
-    
-    const inputCategoryName = document.querySelector("#categoryName") 
+    const inputCategoryName = document.querySelector("#categoryName")
     saveCategoryButton.addEventListener("click", function(){
         const SaveCategoryName = inputCategoryName.value;
-        db.collection("Categories").add({
-            Category: SaveCategoryName
-        })
-        .then(function(docRef){
-            console.log("Category saved!", docRef.id);
-        })
-        .catch(function(error){
-            console.log("Got and eror: ", error);
-        });
+        inputCategoryName.value='';
+        if(SaveCategoryName=='NIL'||SaveCategoryName=='nil' ||SaveCategoryName=='Nil'){
+            alert("Cannot add a category named NIL")
+        }else{
+            db.collection("Categories").add({
+                Category: SaveCategoryName
+            })
+            .then(function(docRef){
+                console.log("Category saved!", docRef.id);
+                document.getElementById('categoryName').value='';
+                populateSelectDropdownCategory();//repopulate the list
+            })
+            .catch(function(error){
+                console.log("Got and eror: ", error);
+            });
+        }
     });
 
-    //TEST BUTTON. To see if something works
     $(document).on("click","#testbtn", function(){
-        
-    //  alert(selectedCategory)
+        alert("testbutton")
     })
+/////////////////////////////////////////////////////////////////////////////////////////////
+//Functionality to disable delete button is NIL is selected
+$("#categoryDeleteDrop").change(function(){
+    console.log("category changed")
+    var selectedID = $("#categoryDeleteDrop option:selected").val()
+    if(selectedID=='WQxMI5w2TVYh9xXP0ivH'){
+        $('#buttonDeleteCategory').prop('disabled', true);
+        $("#buttonDeleteCategory").html('This category cannot be deleted');
+    }else{
+        $('#buttonDeleteCategory').prop('disabled', false);
+        $('#buttonDeleteCategory').html('Delete Category')
+    }
+})
 
-//});
+//Delete category functionality
+buttonDeleteCategory.addEventListener("click", function(){
+//WQxMI5w2TVYh9xXP0ivH
+    var selectedDeleteID = $("#categoryDeleteDrop option:selected").val()
+    var selectedDeleteText = $("#categoryDeleteDrop option:selected").text()
+    // console.log(selectedDeleteID)
+    // console.log(selectedDeleteText)
+    //delete from the database:
+    db.collection("Categories").doc(selectedDeleteID).delete().then(function() {
+        console.log("Category successfully deleted!");
+        populateSelectDropdownCategory();//repopulate the list
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+
+});
+
+
+
+    // Below is just to display the table
+    // var productIDs = [];
+    // db.collection("Products")
+    //     .get()
+    //     .then(function(querySnapshot){     
+    //         var content = "";
+    //         querySnapshot.forEach (function(doc){
+    //             content += '<tr>';
+    //             content += '<td>' + doc.data().PName + '</td>';
+    //             content += '<td>' + doc.data().Category + '</td>';
+    //             content += '<td>' + doc.data().Raw_Price + '</td>';
+    //             content += '<td>' + doc.data().Sales_Price + '</td>';
+    //             // content += '<td>' + '<Button>Edit</Button>' + '</td>';
+    //             content += '</tr>';
+    //     })
+    //     $('#table').append(content);
+    // })
+    
+
